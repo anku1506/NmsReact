e020ec48216adb77112f9c1125073864d4b561e1
anku1506 <kumaranku946@gmail.com>
2024-06-19 23:00:27 +0530

    Foundation of jsx and function component basics

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a Namestey React  h1 tag 📌"),
-    React.createElement("h2", {}, "I am a h2 tag")
-  ]),
+//functional component-
+const Title = () => {
+  return <h1>NMSTY React- In title functional component</h1>
+}
 
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-    
-  ]),
-]);
+
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+//Below three produces same result to call title component
+      <Title />
+      {Title()}
+      <Title></Title>
+      
+      <h1 className="heading"> from functional component</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>);
